6ed91d66446efa6fc901ddb325b452bdf78fba26
Nikolay <nikolay.malygin@gmail.com>
2022-05-20 02:04:07 +0700

    init commit

---
+++ src/main/java/ru/malygin/parser_client/KinopoiskParserClientApplication.java
@@ -0,0 +1,13 @@
+package ru.malygin.parser_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinopoiskParserClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinopoiskParserClientApplication.class, args);
+    }
+
+}
